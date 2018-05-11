5923cb2693521080385ed90c3d3299e738e71f5c
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-05-11 15:54:32 +0300

    Update warden configuration and minio.sh

--- ext-bin/minio.sh
+++ ext-bin/minio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -z "$(MAPR_MINIO_CONF_DIR)" ]
+if [ -z "$(MINIO_DIR)" ]
 then
    echo ""
    exit 1
@@ -9,19 +9,19 @@ fi
 case $1 in
     start)
         echo "Running minio"
-        $(MAPR_SERVER_DIR)/minio --config-dir $(MAPR_MINIO_CONF_DIR) -T $(MAPR_MINIO_CONF_DIR)/tenants.json $(MAPR_MINIO_MOUNT_DIR) & echo $! > $(MAPR_MINIO_PID_FILE)
+        $(MINIO_DIR)/bin/minio --config-dir $(MINIO_DIR)/conf -T $(MINIO_DIR)/conf/tenants.json $(MINIO_DIR)/conf & echo $! > $(MINIO_PID_FILE)
         ;;
     stop)
-        if [ -f $(MAPR_MINIO_PID_FILE)]
+        if [ -f $(MINIO_PID_FILE)]
         then
             echo "Stopping minio"
-            cat $(MAPR_MINIO_PID_FILE) | xargs kill -9
+            cat $(MINIO_PID_FILE) | xargs kill -9
         else
             echo "Minio is not running"
         fi
         ;;
     status)
-        if [ -f $(MAPR_MINIO_PID_FILE)]
+        if [ -f $(MINIO_PID_FILE)]
         then
             echo "Minio is running"
         else
