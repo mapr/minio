e11750bdb86356d5275e712f98b0f257e04e0497
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-05-15 12:23:55 +0300

    Fix minio.sh runner for Warden

--- ext-bin/minio.sh
+++ ext-bin/minio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -z "$(MINIO_DIR)" ]
+if [ ! -d $MINIO_DIR ]
 then
    echo ""
    exit 1
@@ -9,21 +9,22 @@ fi
 case $1 in
     start)
         echo "Running minio"
-        $(MINIO_DIR)/bin/minio --config-dir $(MINIO_DIR)/conf -T $(MINIO_DIR)/conf/tenants.json $(MINIO_DIR)/conf & echo $! > $(MINIO_PID_FILE)
+        $MINIO_DIR/bin/minio --config-dir $MINIO_DIR/conf -T $MINIO_DIR/conf/tenants.json $MINIO_DIR/conf & echo $! > $MINIO_PID_FILE
         ;;
     stop)
-        if [ -f $(MINIO_PID_FILE)]
+        if [ -f $MINIO_PID_FILE ]
         then
             echo "Stopping minio"
-            cat $(MINIO_PID_FILE) | xargs kill -9
+            cat $MINIO_PID_FILE | xargs kill -9
         else
             echo "Minio is not running"
         fi
         ;;
     status)
-        if [ -f $(MINIO_PID_FILE)]
+        if [ -f $MINIO_PID_FILE ]
         then
             echo "Minio is running"
         else
             echo "Minio is not running"
         fi
+esac
