e3ac1030a7350ea25225da3d8ef82d5329635af8
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-05-15 16:18:22 +0300

    Fix minio run command in minio.sh

--- ext-bin/minio.sh
+++ ext-bin/minio.sh
@@ -9,7 +9,7 @@ fi
 case $1 in
     start)
         echo "Running minio"
-        $MINIO_DIR/bin/minio --config-dir $MINIO_DIR/conf -T $MINIO_DIR/conf/tenants.json $MINIO_DIR/conf & echo $! > $MINIO_PID_FILE
+        $MINIO_DIR/bin/minio server --config-dir $MINIO_DIR/conf -T $MINIO_DIR/conf/tenants.json $MINIO_DIR/conf & echo $! > $MINIO_PID_FILE
         ;;
     stop)
         if [ -f $MINIO_PID_FILE ]
