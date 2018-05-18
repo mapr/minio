4c063a432bf0976eb8496f3a70335e6484727d2e
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-05-18 18:56:16 +0300

    Fix configure.sh script

--- ext-bin/configure.sh
+++ ext-bin/configure.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
 
-cp /opt/mapr/s3server/s3server-1.0.0/conf/warden/s3server.conf /opt/mapr/conf/conf.d
+cp /opt/mapr/s3server/s3server-1.0.0/conf/warden.s3server.conf /opt/mapr/conf/conf.d
