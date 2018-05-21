6fdb490db6abff00069814d8a9b53b40b9ee9ec1
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-05-21 12:46:06 +0300

    Set correct permission bits in configure.sh

--- ext-bin/configure.sh
+++ ext-bin/configure.sh
@@ -1,3 +1,8 @@
 #/usr/bin/env sh
 
-cp /opt/mapr/s3server/s3server-1.0.0/conf/warden.s3server.conf /opt/mapr/conf/conf.d
+WARDEN_CONF=/opt/mapr/s3server/s3server-1.0.0/conf/warden.s3server.conf
+MINIO_BINARY=/opt/mapr/s3server/s3server-1.0.0/bin/minio
+
+cp $WARDEN_CONF /opt/mapr/conf/conf.d
+chown mapr $MINIO_BINARY
+chmod 6050 $MINIO_BINARY
