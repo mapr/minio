9a429b4cbea3f8933f4ed00723135c2948d5a205
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-06-19 14:21:35 +0300

    [S3-77] s3server issues with tenant.json and minio.json

--- ext-bin/configure.sh
+++ ext-bin/configure.sh
@@ -24,6 +24,7 @@ function tweakPermissions() {
     chown ${MAPR_USER}:${MAPR_GROUP} $S3SERVER_HOME
     chown ${MAPR_USER}:${MAPR_GROUP} $S3SERVER_HOME/bin
     chmod 6150 $MINIO_BINARY
+    chmod 700 $S3SERVER_HOME/conf/tenants.json
 }
 
 function setupCertificate() {
