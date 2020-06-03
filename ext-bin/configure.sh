e368df671b0100c5d28cb5039998af13b84665ed
Artem Chernatsky <achernatsky@mapr.com>
2020-06-03 14:24:51 +0300

    [S3-180] Added chown for util folder

--- ext-bin/configure.sh
+++ ext-bin/configure.sh
@@ -105,6 +105,7 @@ function tweakPermissions() {
     chown -R ${MAPR_USER}:${MAPR_GROUP} $OBJECTSTORE_HOME/conf
     chown ${MAPR_USER}:${MAPR_GROUP} $OBJECTSTORE_HOME
     chown -R ${MAPR_USER}:${MAPR_GROUP} $OBJECTSTORE_HOME/bin
+    chown -R ${MAPR_USER}:${MAPR_GROUP} $OBJECTSTORE_HOME/util
     chmod 6150 $MINIO_BINARY
     setcap "cap_setuid,cap_setgid+eip" $MINIO_BINARY
     chmod 700 $MAPR_S3_CONFIG
